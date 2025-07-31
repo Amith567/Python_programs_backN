28559ed448a5b25003ebcb1bad2135436d0d1df0
Amith P <amithanilp@gmail.com>
2025-07-31 12:14:01 +0530

    validation added

--- authentication.py
+++ authentication.py
@@ -1,20 +1,35 @@
 def validate_email(email):
-    return True
-
+    if '@'not in email:
+        return False,"it doesn't contain '@' "
+    name,domain=email.split('@',1)
+    if '.'not in domain:
+        return False,"it doesn't contain '.' "
+    if not name or not domain:
+        return False,"name or domain is null "
+    return True,"email is valid"
 def validate_password(password):
-    return True
+    if len(password)<8:
+        return False,"password length is less than 8 "
+    if not any(digi.isdigit() for digi in password):
+        return False,"your password doesn't contain a digit "
+    if not any(let.isalpha() for let in password):
+        return False,"your password doesn't contain a alphabet"
 
-email=input("Enter email : ")
+    return True,"password is valid"
+
+# email=input("Enter email : ")
+email="nived@gmail.com"
 password=input("Enter password : ")
 
-maill=validate_email(email)
-passw=validate_password(password)
+maill,mail_msg=validate_email(email)
+passw,passw_msg=validate_password(password)
 
 if maill==True and passw==True:
     print(f"you logined successfully !.")
-elif maill==False and passw==False:
-    print(f"your email and password is in incorrect format !.")
-elif maill==False:
-    print(f"your email is in incorrect format !.")
-else:
+elif  maill==True and passw==False:
+    print(f"your password is in incorrect format !.{passw_msg}")
+    
+elif maill==False and passw==True:
+    print(f"your email is in incorrect format !.{mail_msg}")
+else:    
     print(f"your email and password is in incorrect format !.")
